8e296685537bae9efe4d50fa9b7d0468bb1539cc
steephan-s <steephanselvaraj@testpress.in>
2016-04-23 17:28:28 +0530

    Change api location, add backbone_js frontend
    api location changed, add a backbone_js frontend and urls updated.

--- todoapp/todo/api/v1/views.py
+++ todoapp/todo/api/v1/views.py
@@ -1,7 +1,11 @@
 from todo.models import Task
 from todo.serializers import TaskSerializer
 from rest_framework import generics
+from django.shortcuts import render
 
 class index(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+def detail(request):
+     return render(request, 'todo/detail.html')
--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from todo import views
 
 urlpatterns = [
     url(r'^api/v1/todo/', include('todo.urls')),
+    url(r'^index/', views.detail, name='detail'),
 ]

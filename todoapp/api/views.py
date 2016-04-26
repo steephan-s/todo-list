ae84206380d4dab13e9f0828687f8598c73f268e
steephan-s <steephanselvaraj@testpress.in>
2016-04-26 18:13:17 +0530

    Replace SQL lite with PostgreSQL.
    Remove SQL lite and add PostgreSQL, Remove Testing code lines.

--- todoapp/api/views.py
+++ todoapp/api/views.py
@@ -4,11 +4,9 @@ from rest_framework import generics
 from django.shortcuts import render
 
 class index(generics.ListCreateAPIView):
-    print("We are inside the index class")
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     
 class details(generics.RetrieveUpdateDestroyAPIView):
-    print("We are inside the details class")
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
